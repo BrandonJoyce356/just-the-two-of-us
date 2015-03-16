51d17f0ccd40f666a37e656d1b6de0296579949f
Ryan Bone <rbone@covermymeds.com>
2015-03-16 09:50:49 -0400

    show the stuff

--- question_filter/talk.sh
+++ question_filter/talk.sh
@@ -3,6 +3,7 @@ clear
 
 echo "colorscheme github" >> ~/.vimrc
 
+vim version_1.rb
 vimdiff version_1.rb version_2.rb
 vimdiff version_2.rb version_3.rb
 vimdiff version_3.rb version_4.rb
@@ -11,3 +12,4 @@ vimdiff version_5.rb version_6.rb
 vimdiff version_6.rb version_7.rb
 vimdiff version_7.rb version_8.rb
 vimdiff version_8.rb version_9.rb
+vimdiff version_1.rb verison_9.rb
